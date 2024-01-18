d370afd19327eab40c0c074f8302b7594eaa91b7
Caroline Cerussi <caroceru@gmail.com>
2024-01-18 15:25:58 -0800

    create js and readme.md files for address book app

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+//business logic for addressbook
+
+function AddressBook() {
+  this.contacts= {};
+}
+
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}; 
+
